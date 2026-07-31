53ae594f9b2d9937bb364639b485466743fb9c5d
JJEII <70997819+JJEII@users.noreply.github.com>
2026-07-31 12:47:37 -0600

    Cleaned up

--- two_ints.py
+++ two_ints.py
@@ -1,11 +1,10 @@
 from math import sqrt
-from typing import Union
 
 def is_prime(n: int) -> bool:
     """Determine if number `n` is prime or not."""
     if not isinstance(n, int) or n < 2:
         return False
-    primes = [2, 3, 5, 7]
+    primes = [2]
     while n > primes[-1]:
         prev_last = primes[-1]
         nxt = primes[-1]
@@ -18,16 +17,16 @@ def is_prime(n: int) -> bool:
                 primes.append(nxt)
     return n in primes
     
-def assess_1(ab_sum: int) -> Union[None, list[int, int]]:
+def assess_1(ab_sum: int) -> list[tuple[int, int]]:
     """Assess Statement 1.
     Return a list containing all partitions of `ab_sum` that FAIL Statement 1."""
     found_partitions = []
     for A in range(2, ab_sum//2+1):
         if is_prime(A) and (is_prime(ab_sum-A) or A*A==ab_sum-A):
-            found_partitions.append([A, ab_sum-A])
+            found_partitions.append( (A, ab_sum-A) )
     return found_partitions
 
-def assess_2(ab_prod: int) -> Union[list[list[int, int]], list[list[int, int], list[int, int]]]:
+def assess_2(ab_prod: int) -> list[tuple[int, int]]:
     """Assess Statement 2.
     Return a list containing all factor-pairs of `ab_prod` that PASS Statement 1 when summed."""
     found_factor_pairs = []
@@ -35,33 +34,31 @@ def assess_2(ab_prod: int) -> Union[list[list[int, int]], list[list[int, int], l
         if ab_prod%A == 0:
             B = ab_prod//A
             if len(assess_1(A+B)) == 0:
-                found_factor_pairs.append([A, B])
+                found_factor_pairs.append( (A, B) )
     return found_factor_pairs
 
-def assess_3(ab_sum: int) -> Union[list[list[int, int]], list[list[int, int], list[int, int]]]:
-    """Assess Statement 3.
-    Assumes this `ab_sum` already passed Statement 1.
+def assess_3(ab_sum: int) -> list[tuple[int, int]]:
+    """Assess Statement 3. (Assumes this `ab_sum` already passed Statement 1.)
     Return a list containing all partitions of `ab_sum` that PASS Statement 2 when multiplied."""
     found_partitions = []
     for A in range(2, ab_sum//2+1):
         B = ab_sum - A
         result2 = assess_2(A*B)
         if len(result2) == 1: # anything length != 1 is a failure
-            found_partitions.append(result2[0]) # flatten it
+            found_partitions.append(result2[0]) # flatten it (only one pair in there)
             if len(found_partitions) > 1:
                 return found_partitions
     return found_partitions
 
-RED   = f"\x1b[38;2;{200};{0};{0}m"
-GREEN = f"\x1b[38;2;{0};{180};{0}m"
-RESET = "\x1b[0m"
-tab = "   "
+PASS, FAIL = [f"\x1b[38;2;{r};{g};{b}m{sym}\x1b[0m" for sym, r, g, b in [["✓", 0, 180, 0], ["×", 200, 0, 0]]]
 for s in range(4, 101): # smallest conceivable sum is 4 (when A=2 and B=2)
     if len(result1 := assess_1(s)) == 0: # if statement 1 succeeded
         if len(result3 := assess_3(s)) == 1: # if statement 3 succeeded
-            print(f"{GREEN}✓{RESET} Sum={s} passes Statement 3. Integers A and B are {' and '.join(map(str, result3[0]))}.")
+            details = " and ".join(map(str, result3[0]))
+            print(f"{PASS} Sum={s} passes Statement 3. Integers A and B are {details}.")
         else: # statement 3 failed (number of possible factor-pairs != 1)
-            status = ", ".join(str(AB) for AB in result3) if len(result3)>0 else "(none)"
-            print(f"{tab}{RED}×{RESET} Sum={s} fails Statement 3 because these factor-pairs pass Statement 2: {status}.")
+            details = ", ".join(str(AB) for AB in result3) if len(result3)>0 else "(none)"
+            print(f"   {FAIL} Sum={s} fails Statement 3 because these factor-pairs pass Statement 2: {details}.")
     else: # statement 1 failed
-        print(f"{tab}{tab}{RED}×{RESET} Sum={s} fails Statement 1 because of these partitions: {', '.join(map(str, result1))}.")
+        details = ", ".join(map(str, result1))
+        print(f"      {FAIL} Sum={s} fails Statement 1 because of these partitions: {details}.")
